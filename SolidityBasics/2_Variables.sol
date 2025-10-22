4e159630a2e68654493cb44d07607057ec6771b3
Seham Othman <210709718@posta.mu.edu.tr>
2025-10-22 22:28:06 +0300

    Update variable file
    Refactor contract structure and variable definitions.

--- SolidityBasics/2_Variables.sol
+++ SolidityBasics/2_Variables.sol
@@ -1,27 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Variables {
-    // State Variables
-    int256 public myInt = 1;
-    uint256 public myUint256 = 1;
-    uint8 public myUint8 = 1;
-
-    string public myString = "Hello, World!";
-    bytes32 public myBytes32 = "Hello, World!";
-
-    address public myAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
-    struct MyStruct {
-        uint256 myUint256;
-        string myString;
+pragma solidity ^0.8.0; //telling the compiler what version we will use!
+contract MyContract {
+    //Variables : ways to store informations insode my smart contract to reuse it!
+    // State Variables vs Local Variables :
+    //1-State Variables : Scope is inside the entire contract!
+    uint myUint = 1;
+    function getMyUnit() public returns(uint){
+        myUint++;
+        return myUint;
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello, World!");
 
-    // Local Variables
-    function getValue() public pure returns (uint256) {
-        uint256 value = 1;
+    //2-Local Variables : Variables exist inside solidity functions.
+    function getValue() public pure returns(uint){
+        //Local Variable Example: this var only reachable inside this function! Variable Scope!
+        uint value =1;
         return value;
     }
+
 }
